e51f3ece6f2b6f7afabb73d665605d0e1b84bef8
Doyle <alexhaoze@hotmail.com>
2019-05-31 11:28:37 +0800

    1.0

---
+++ NonConnectionException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Ls.Core
+{
+    class NonConnectionException:Exception
+    {
+        public NonConnectionException(string mes, Exception innerException)
+            : base(mes, innerException) 
+        {
+            
+        }
+    }
+}
